ff45925c229e939a1dbe320d14c7c46e2ea2df64
TMenezesO <43683178+TMenezesO@users.noreply.github.com>
2018-10-27 00:01:37 -0300

    Update Aula6_twiki2.py

--- Aula6/Aula6_twiki2.py
+++ Aula6/Aula6_twiki2.py
@@ -1,22 +1,48 @@
-import turtle
+mport turtle
 import math
-sammy = turtle.Turtle()
-jn = turtle.Screen()         
-jn.bgcolor("white")
 
-#cores = ["yellow", "orange", "purple", "lightblue","violet","white"]
+cores = ["yellow", "orange", "purple", "lightblue","violet","white"]
 
+# DIferenças para estrelas com n de pontas pares ou ímpares
 
-def estrela(x,color):
+def estrela_impar(x,color): # funciona apenas para estrelas com n de pontas ímpar
+    sammy = turtle.Turtle()
+    jn = turtle.Screen()         
+    jn.bgcolor("white")
+    sammy.setpos((0,0))
+# funciona apenas para número de pontas ímpar
     for i in range(x):
         sammy.color(color)
         sammy.speed(0)
-        sammy.pensize(3)
-        sammy.left((180-((180*(x-2))/x))*2)
-        sammy.forward(50)
+        sammy.pensize(2)
+        sammy.left(180.0 - (360.0/(2*x)))
+        sammy.forward(250)
 
-#    ((x - 2)*180/x)
        
     jn.mainloop()
+    
+#estrela_impar(29,"red")
 
-estrela(23,"red")
+def estrela(n,cor):
+    sammy = turtle.Turtle()
+    jn = turtle.Screen()         
+    jn.bgcolor("white")
+    sammy.setpos((0,0))
+    
+    for i in range(n):
+        if n % 2 == 0:
+            sammy.color(cor)
+            sammy.speed(0)
+            sammy.pensize(2)
+            sammy.left(180.0 - (360.0/(n)))
+            sammy.forward(250)
+    
+        else:
+            sammy.color(cor)
+            sammy.speed(0)
+            sammy.pensize(2)
+            sammy.left(180.0 - (360.0/(2*n)))
+            sammy.forward(250)
+    
+    jn.mainloop()
+estrela(13,"green")
