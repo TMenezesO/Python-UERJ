a6e437973743c637176edb2d3607fcdbb486cd91
TMenezesO <43683178+TMenezesO@users.noreply.github.com>
2018-11-15 17:12:30 -0200

    Update proj.py

--- proj.py
+++ proj.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 g=9.81
 
-def pend_mola(xo,zo,k,m,lo,r):
+def pend_mola(xo,zo,k,m,lo):
+    to=0
     x=xo
-    #x=lo*math.cos(theta)
     z=zo
-    #x=lo*math.sin(theta)
     vx=0
     vz=0
     
@@ -20,63 +19,71 @@ def pend_mola(xo,zo,k,m,lo,r):
             
     lista_ax=[]
     lista_az=[]
+    
+    lista_a=[]
+    lista_v=[]
+    lista_d=[]
+    
+    t1 = [i/100 for i in range(0,1000)]
+    dt = t1[1] - t1[0]
 
-    for i in range(0,10,1):
-        #for n in range(0,10,1):
-        theta=i
-        x=lo*math.cos(theta)
-        z=lo*math.sin(theta)
+    for t in t1:
+
+        ax = -(k/m)*((math.sqrt(math.pow(x,2) + math.pow(z,2)))-lo)*(x/(math.sqrt(math.pow(x,2) + math.pow(z,2))))
+        az = -g -(k/m)*((math.sqrt(math.pow(x,2) + math.pow(z,2)))-lo)*(z/(math.sqrt(math.pow(x,2) + math.pow(z,2))))
         
-        ax = -(k/m)*(r-lo)*math.sin(theta)
-        az= -g + (k/m)*(r-lo)*math.cos(theta)
-        for n in range(0,10,1):
-            t=n
-            #ax = -(k/m)*(r-lo)*math.sin(theta)
-            #az= -g + (k/m)*(r-lo)*math.cos(theta)
+        # consertar t, deve ser o intervalo de tempo 
+        
+        vx = vx + ax*dt
+        vz = vz + az*dt
             
-            vx = vx + ax*t
-            vz = vz + az*t
+        x = x + vx*dt
+        z = z + vz*dt
+        
             
-            x = x + vx*t
-            z = z + vz*t
-            
-            lista_ax.append(ax)
-            lista_az.append(az)
+        lista_ax.append(ax)
+        lista_az.append(az)
 
-            lista_vx.append(vx)
-            lista_vz.append(vz)
+        lista_vx.append(vx)
+        lista_vz.append(vz)
             
-            lista_x.append(x)
-            lista_z.append(z)
+        lista_x.append(x)
+        lista_z.append(z)
             
-            a = math.sqrt(math.pow(ax,2) + math.pow(az,2))
-            v = math.sqrt(math.pow(vx,2) + math.pow(vz,2))
-            d = math.sqrt(math.pow(x,2) + math.pow(z,2))
+        a = math.sqrt(math.pow(ax,2) + math.pow(az,2))
+        v = math.sqrt(math.pow(vx,2) + math.pow(vz,2))
+        d = math.sqrt(math.pow(x,2) + math.pow(z,2))
+        
+        lista_a.append(a)
+        lista_v.append(v)
+        lista_d.append(d)
             
-            print(lista_x)
-            print(lista_z)
-            #print(math.cos(theta))
-            #print((r-lo))
-    plt.plot(t,d)
+        #print(lista_vx)
+        #print(lista_vz)
+
+    plt.plot(lista_d,t1)
     plt.xlabel("tempo s")
     plt.ylabel("distancia m")
+    plt.show()
     
-    plt.plot(t,v)
+    plt.plot(lista_v,t1)
     plt.xlabel("tempo s")
     plt.ylabel("velocidade m/s")
     
-    plt.plot(t,a)
+    plt.plot(lista_a,t1)
     plt.xlabel("tempo s")
     plt.ylabel("aceleracao m/s^2")
+    plt.show()
     
     
     
-    plt.plot(vx,x)
+    plt.plot(lista_vx,lista_x)
     plt.xlabel("velocidade m/s")
     plt.ylabel("distancia m")
+    plt.show()
     
-    
-    plt.plot(vz,z)
+  
+    plt.plot(lista_vz,lista_z)
     plt.xlabel("velocidade m/s")
     plt.ylabel("distancia m")
     
@@ -88,69 +95,5 @@ def pend_mola(xo,zo,k,m,lo,r):
                 f.write("\t")
                 f.write("%s\n" % az)
                 
-pend_mola(0,0,1,1,1,2)
-
-
-def pend_simples():
-    x=0
-    #x=lo*math.cos(theta)
-    z=0
-    #x=lo*math.sin(theta)
-    vx=0
-    vz=0
-    
-    lista_vx=[]
-    lista_vz=[]
-            
-    lista_x=[]
-    lista_z=[]
-            
-    lista_ax=[]
-    lista_az=[]
-
-    for i in range(0,180,1):
-        theta=i
-        x=lo*math.cos(theta)
-        z=lo*math.sin(theta)
-        for n in range(0,60,1):
-            t=n
-            g=9.81
-            ax = -g*math.cos(theta)*math.sin(theta)
-            az = -g + g*math.pow((math.cos(theta)),2)
-            
-            lista_ax.append(ax)
-            lista_az.append(az)
-        
-            vx = vx + ax*t
-            vz = vz + az*t
-            lista_vx.append(vx)
-            lista_vz.append(vz)
-
-            x = x + vx*t
-            z = z + vz*t
-            
-                        
-            v = math.sqrt(math.pow(vx,2) + math.pow(vz,2))
-            
-            d = math.sqrt(math.pow(x,2) + math.pow(z,2))
-            
-            
-            lista_x.append(x)
-            lista_z.append(z)
-            
-            print(lista_ax)
-            print(lista_az)
-            
-    plt.plot(t,x)
-    plt.xlabel("tempo s")
-    plt.ylabel("distancia m")
-    
-    plt.plot(t,z)
-    plt.xlabel("tempo s")
-    plt.ylabel("distancia m")
-            
-            #print(lista_vx)
-            #print(lista_vz)
-            #print(lista_x)
-            #print(lista_z)
+pend_mola(1,1,1,1,1)
 
