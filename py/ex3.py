d2cd04c091132c32073b76f6dc82827a2fa0d1ba
TMenezesO <43683178+TMenezesO@users.noreply.github.com>
2018-12-06 11:12:34 -0200

    Delete ex3.py

--- py/ex3.py
+++
@@ -1,8 +0,0 @@
-
-
-def custo(precolivro,nlivros,custo1,custoli,desc):
-   custototal=nlivros*(precolivro*desc) + custo1 + (custoli*(nlivros-1))
-   print (custototal)
-
-custo(24.95,60,3,0.75,0.6)
-#custo(30,10,3,0.75,1)
